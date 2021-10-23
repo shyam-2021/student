d672e4664f22efc8743f45e64aca61668368aa78
shyam-2021 <shyamsundarj.20cse@kongu.edu>
2021-10-23 11:01:44 +0530

    firstcommit

---
+++ details.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+	void main()
+{ 
+	string name,rollno,fathername,mothername,email,address;
+	int age,mobile number,blood group,aadhar number; 
+	
+	printf("enter the name");
+	
+	scanf("%s",&name); 
+	printf("name is",name);
+	printf("enter the rollno");
+	scanf("%s",&rollno); 
+	printf("roll no is",rollno);
+	printf("enter the fathername");
+	scanf("%s",&fathername);
+	printf("fathername is:",fathername);
+	printf("enter the mothername");
+	scanf("%s",&mothername);
+	printf("mother name",mothername);
+	printf("enter the email);
+	scanf("%s",&email);
+	printf("email",email);
+	printf("enter the addres");
+	scanf("%s",&address);
+	printf("address",address);
+	printf("enter the age");
+	scanf("%d",&age);
+	printf("age :",age)'
+	printf("enter the mobile number ");
+	scanf("%d",&mobile number);
+	printf("mobile number",mobile number);
+	printf("enter the blood group");
+	scanf("%c",&blood group);
+	printf("bloodgroup",bloodgroup);
+	printf("enter the aadhar number");
+	scanf("%d",&aadhar number);
+	printf("aadhar number",aadhar number);
+}
+
+
