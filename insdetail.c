69ad776c779abcc0bf144c8072aee12f2c4d62a7
shyam-2021 <shyamsundarj.20cse@kongu.edu>
2021-10-23 11:13:43 +0530

    insurance commit

---
+++ insdetail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	printf("name is shyam");
+	printf("rollno is 20csr196);
+	printf("nominee name:sunder");
+	printf("nominee age is 20)";
+	printf("mobile number is9487765666");
+	printf("occupation is student");
+	printf("email is shyamsunder@gmail.com);
+	printf("aadhar number is 909865688434);
+	}
