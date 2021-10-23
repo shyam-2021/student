b053dce7f09de5d3dc44215c693ddde9c9769cb2
shyam-2021 <shyamsundarj.20cse@kongu.edu>
2021-10-23 11:24:00 +0530

    placement commit

---
+++ placement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+{
+	void main()
+	{
+		printf("name is shyam");
+		printf("roll no is 20csr196");
+		printf("mobile number is 9876646708");
+		printf("personal mail id is shyamsunderj@gmail.com);
+		printf("edu mailid is shyamsunderj.20cse@kongu.edu);
+		printf("hosteller");
+		printf("number of arrers: 0");
+		printf("gpa in first semester 9.91");
+		printf("gpa in second semester 9.70");
+		printf("gpa in third semester 9.95");
+		printf("cgpa upto third semester 9.5");
+	}
