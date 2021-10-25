8f6be8f021569ef570b5b863856e60cf8746a75a
shyam-2021 <shyamsundarj.20cse@kongu.edu>
2021-10-25 19:12:07 +0530

    activity commit

---
+++ activitydetail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	printf("roll no is 20196");
+	printf("semester:3");
+	printf("no of paper presented inside KEC:1");
+	printf("no of projects presented inside KEC:1");
+	printf("no of papers presented outside KEC:2");
+	printf("no of projects presented outside KEC:1");
+	printf("nof prize won in coding carnival:2");
+	printf("no of prize won in hackathon:1");
+}
