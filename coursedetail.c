62a9d678984b7b3cdbad952aee0f9ea22e36501b
shyam-2021 <shyamsundarj.20cse@kongu.edu>
2021-10-25 19:19:26 +0530

    course complete

---
+++ coursedetail.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	printf(" rollno is 567567");
+	printf("semester : 3");
+	printf("no of credited courses completed:1");
+	printf("name of one credit course completed is: git");
+	printf("no of value added courses completed:2");
+	printf("name of value added course:java");
+}
+
